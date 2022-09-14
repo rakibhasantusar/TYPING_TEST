15c2f81949805359103101679f9e34b58ab64db4
Rakibul Hasan <39860864+rakibhasantusar@users.noreply.github.com>
2022-09-15 00:07:44 +0600

    second gula purno shonkha done

--- script.js
+++ script.js
@@ -67,7 +67,8 @@ const gameOver = () => {
   // the current time is the finish time
   // so total time taken is current time - start time
   const finishTime = new Date().getTime();
-  const timeTaken = (finishTime - startTime) / 1000;
+  const timeround1 = (finishTime - startTime) / 1000;
+  const timeTaken = (Math.round(timeround1));
 
   // show result modal
   resultModal.innerHTML = "";
@@ -133,8 +134,10 @@ displayHistory();
 // Show typing time spent
 setInterval(() => {
   const currentTime = new Date().getTime();
-  const timeSpent = (currentTime - startTime) / 1000;
+  const timeround = (currentTime - startTime) / 1000;
+  const timeSpent = (Math.round(timeround));
 
 
+  // document.getElementById("show-time").innerHTML = `${startTime ? timeround : 0} seconds`;
   document.getElementById("show-time").innerHTML = `${startTime ? timeSpent : 0} seconds`;
 }, 1000);
